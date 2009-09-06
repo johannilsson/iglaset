20dd5c4e2430fa54273a5cdfdb1b445ca4995a74
Johan Nilsson <github@markupartist.com>
2009-09-06 10:53:33 +0200

    added about dialog

--- src/com/markupartist/iglaset/activity/StartActivity.java
+++ src/com/markupartist/iglaset/activity/StartActivity.java
@@ -1,7 +1,15 @@
 package com.markupartist.iglaset.activity;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -9,6 +17,9 @@ import android.widget.Button;
 import com.markupartist.iglaset.R;
 
 public class StartActivity extends Activity {
+    private static final int DIALOG_ABOUT = 0;
+    private static final String TAG = "StartActivity";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,5 +39,44 @@ public class StartActivity extends Activity {
     public boolean onSearchRequested() {
         startSearch(null, false, null, false); 
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                showDialog(DIALOG_ABOUT);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case DIALOG_ABOUT:
+            PackageManager pm = getPackageManager();
+            String version = "";
+            try {
+                PackageInfo pi = pm.getPackageInfo(this.getPackageName(), 0);
+                version = pi.versionName;
+            } catch (NameNotFoundException e) {
+                Log.e(TAG, "Could not get the package info.");
+            }
+
+            Dialog aboutDialog = new Dialog(this);
+            aboutDialog.setCanceledOnTouchOutside(true);
+            aboutDialog.setContentView(R.layout.about_dialog);
+            aboutDialog.setTitle(getText(R.string.app_name) + " " + version);
+            return aboutDialog;
+        }
+        return null;
     }
 }
