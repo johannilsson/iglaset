498ed0962557878256f227817182c6d3dd95b8a3
Johan Nilsson <github@markupartist.com>
2010-02-17 22:10:37 +0100

    added user ratings and recommendations to the list

--- src/com/markupartist/iglaset/activity/CategoryActivity.java
+++ src/com/markupartist/iglaset/activity/CategoryActivity.java
@@ -1,18 +1,39 @@
 package com.markupartist.iglaset.activity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.markupartist.iglaset.R;
+import com.markupartist.iglaset.activity.SectionedAdapter.Section;
 import com.markupartist.iglaset.util.Tracker;
 
 public class CategoryActivity extends ListActivity {
+    private static final String TAG = "CategoryActivity";
+    SectionedAdapter mSectionedAdapter = new SectionedAdapter() {
+        protected View getHeaderView(Section section, int index, 
+                View convertView, ViewGroup parent) {
+            TextView result = (TextView) convertView;
+
+            if (convertView == null)
+                result = (TextView) getLayoutInflater().inflate(R.layout.header, null);
+
+            result.setText(section.caption);
+            return (result);
+        }
+    };
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,19 +46,45 @@ public class CategoryActivity extends ListActivity {
 
         ArrayAdapter<Category> categoryAdapter = new ArrayAdapter<Category>(
                 this, R.layout.category_list_row, createCategories());
-        this.setListAdapter(categoryAdapter);
+
+        mSectionedAdapter.addSection(0, "Mina listor", createUserListAdapter());
+        mSectionedAdapter.addSection(1, "Kategorier", categoryAdapter);
+
+        this.setListAdapter(mSectionedAdapter);
     }
 
+    private SimpleAdapter createUserListAdapter() {
+        ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        Map<String, Object> recomendationMap = new HashMap<String, Object>();
+        recomendationMap.put("text", "Rekommendationer");
+        Map<String, Object> gradeMap = new HashMap<String, Object>();
+        gradeMap.put("text", "Betygsatta artiklar");
+        list.add(recomendationMap);
+        list.add(gradeMap);
+
+        return new SimpleAdapter(this, list, 
+                R.layout.category_list_row,
+                new String[] { "text" },
+                new int[] { 
+                    R.id.category_list_row
+                });
+    }
+    
     /** Called when the list is clicked. */
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        Category category = (Category) getListAdapter().getItem(position);
+        Section section = mSectionedAdapter.getSection(position);
+        if (section.id == 1) {
+            Category category = (Category) getListAdapter().getItem(position);
 
-        Intent i = new Intent(this, SearchResultActivity.class);
-        i.putExtra(SearchResultActivity.EXTRA_SEARCH_CATEGORY_ID, category.getId());
-        startActivity(i);
+            Intent i = new Intent(this, SearchResultActivity.class);
+            i.putExtra(SearchResultActivity.EXTRA_SEARCH_CATEGORY_ID, category.getId());
+            startActivity(i);
+        } else {
+            Log.d(TAG, "fix search for this...");
+        }
     }
 
     @Override
