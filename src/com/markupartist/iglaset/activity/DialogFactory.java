3e3604cbd51612d01d6d2e78ecba90cc8b795553
Marco Alanen <denravonska@gmail.com>
2010-02-26 03:09:54 +0800

    - Removed dead code which might have caused FC when the program was bullied.

--- src/com/markupartist/iglaset/activity/DialogFactory.java
+++ src/com/markupartist/iglaset/activity/DialogFactory.java
@@ -6,7 +6,6 @@ package com.markupartist.iglaset.activity;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface.OnClickListener;
 
 /**
@@ -14,26 +13,16 @@ import android.content.DialogInterface.OnClickListener;
  *
  */
 public class DialogFactory {
-	
-	public final static int DIALOG_SEARCH_NETWORK_PROBLEM = 0;
-	
+
 	public static Dialog createNetworkProblemDialog(
 			Activity activity,
 			OnClickListener onClickListener) {
-		Dialog dialog = createNetworkProblemDialog((Context) activity, onClickListener);
-		dialog.setOwnerActivity(activity);
-		return dialog;
-	}
-	
-	public static Dialog createNetworkProblemDialog(
-			Context ctx,
-			OnClickListener onClickListener) {
-        return new AlertDialog.Builder(ctx)
+        return new AlertDialog.Builder(activity)
         .setIcon(android.R.drawable.ic_dialog_alert)
         .setTitle("Ett fel inträffade")
         .setMessage("Kunde inte ansluta till servern. Försök igen, eller Cancel för att gå tillbaka till föregående vy.")
         .setPositiveButton("Försök igen", onClickListener)
-        .setNegativeButton(ctx.getText(android.R.string.cancel), onClickListener)
+        .setNegativeButton(activity.getText(android.R.string.cancel), onClickListener)
         .create();
 	}
 }
