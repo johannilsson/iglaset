de2a9fa6a5364c263212c895f7300a521d64dd1a
Marco Alanen <denravonska@gmail.com>
2010-07-28 17:23:07 +0200

    - Added Javadoc comment.

--- src/com/markupartist/iglaset/util/StringUtils.java
+++ src/com/markupartist/iglaset/util/StringUtils.java
@@ -1,9 +1,18 @@
 package com.markupartist.iglaset.util;
 
-import java.util.Iterator;
-import java.util.List;
-
+/**
+ * @author marco
+ * Utility class for string manipulation.
+ */
 public class StringUtils {
+	
+	/**
+	 * Concatenate a list of objects into a delimiter separated string. The objects must be
+	 * convertible to String.
+	 * @param list List to concatenate.
+	 * @param delimiter Optional delimiter
+	 * @return Concatenated string containing the String representations of the objects in list.
+	 */
 	public static String join(Object[] list, String delimiter) {
 		StringBuilder builder = new StringBuilder();
 		for(int i=0; i<list.length; ++i) {
