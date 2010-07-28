482ac0b3113ec955177b1f563a9a4bbd689a9bf0
Marco Alanen <denravonska@gmail.com>
2010-07-28 17:25:26 +0200

    - Removed unused import.

--- src/com/markupartist/iglaset/activity/DrinkDetailActivity.java
+++ src/com/markupartist/iglaset/activity/DrinkDetailActivity.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
