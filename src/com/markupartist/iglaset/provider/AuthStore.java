8343d7deab8794c9d22662fdb5f6086383ec0c92
Marco Alanen <denravonska@gmail.com>
2010-11-08 05:15:01 +0100

    - Removed unused import.

--- src/com/markupartist/iglaset/provider/AuthStore.java
+++ src/com/markupartist/iglaset/provider/AuthStore.java
@@ -2,7 +2,6 @@ package com.markupartist.iglaset.provider;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
