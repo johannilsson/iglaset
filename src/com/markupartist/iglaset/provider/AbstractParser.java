27cf229e578dcc87b00ce2f0cd1dcb7460c3fc1d
Marco Alanen <marco@ormgas.com>
2011-01-04 06:21:33 +0100

    - Moved parsing initialization down to AbstractParser.

--- src/com/markupartist/iglaset/provider/AbstractParser.java
+++ src/com/markupartist/iglaset/provider/AbstractParser.java
@@ -1,15 +1,50 @@
 package com.markupartist.iglaset.provider;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
-public abstract class AbstractParser extends DefaultHandler {
+import android.util.Log;
 
+public abstract class AbstractParser<T> extends DefaultHandler {
+
+	private static final String TAG = AbstractParser.class.getSimpleName();
 	private StringBuilder stringBuilder = new StringBuilder();
 	
 	public abstract void onStartElement(String name, Attributes atts);
 	public abstract void onEndElement(String name, String result);
+	protected abstract ArrayList<T> getContent();
 
+	public ArrayList<T> parse(InputStream in) {
+        InputSource inputSource = new InputSource(in);
+
+        try {
+	        SAXParserFactory spf = SAXParserFactory.newInstance();
+	        SAXParser sp = spf.newSAXParser();
+	        XMLReader xr = sp.getXMLReader();
+	        xr.setContentHandler(this);
+	        xr.parse(inputSource);
+	    } catch (IOException e) {
+	        Log.e(TAG, e.toString());
+	    } catch (SAXException e) {
+	        Log.e(TAG, e.toString());
+	    } catch (ParserConfigurationException e) {
+	        Log.e(TAG, e.toString());
+	    }
+
+		return getContent();
+	}
+	
     public void characters(char ch[], int start, int length) {
     	stringBuilder.append(ch, start, length);
     }	
@@ -24,6 +59,5 @@ public abstract class AbstractParser extends DefaultHandler {
     	onEndElement(name, result);
     	stringBuilder.setLength(0);
     }
-
     
 }
